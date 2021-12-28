ba19afc98c203a79189f3b7ede61f770e2013458
kzim <k.zimny1994@gmail.com>
2021-12-28 17:10:50 -0600

    Added dist folder

--- dist/bundle.06535c4ef1db7cc4.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";console.log("test")})();
-//# sourceMappingURL=bundle.06535c4ef1db7cc4.js.map
---
+++ dist/bundle.24011d215db1965f.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("test"),document.getElementById("copyrightFooterYear").innerHTML=(new Date).getFullYear()})();
+//# sourceMappingURL=bundle.24011d215db1965f.js.map
